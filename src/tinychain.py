1e9f15ce691b09fc7eced7d85dfe1bb063fbfebb
nfl0 <nawfalona@gmail.com>
2023-08-22 03:02:23 +0100

    devving

--- src/tinychain.py
+++ src/tinychain.py
@@ -5,6 +5,13 @@ import random
 
 app = Flask(__name__)
 
+# Placeholder classes for components (to be implemented)
+class ValidationEngine:
+    def validate_and_add_transaction(self, transaction):
+        # Placeholder implementation, validate and return transaction hash
+        transaction_hash = hash(str(transaction))
+        return transaction_hash
+
 class Mempool:
     def __init__(self):
         self.transactions = []
@@ -12,15 +19,13 @@ class Mempool:
     def add_transaction(self, transaction):
         self.transactions.append(transaction)
 
-mempool = Mempool()
-
 class Miner(threading.Thread):
     def __init__(self, mempool):
         super().__init__()
         self.mempool = mempool
-        self.blockchain = []  # A simple list to represent the blockchain
+        self.blockchain = []  # Placeholder for blockchain storage
         self.running = True
-    
+
     def run(self):
         while self.running:
             if self.mempool.transactions:
@@ -30,30 +35,60 @@ class Miner(threading.Thread):
                 print(f"Added transaction '{random_transaction}' to a new block.")
             time.sleep(5)  # Wait for 5 seconds
 
-miner = Miner(mempool)
+# Placeholder class for StorageEngine
+class StorageEngine:
+    def fetch_block(self, block_hash):
+        # Placeholder implementation, fetch block data from database
+        return {"block_hash": block_hash, "transactions": []}
 
-@app.route('/sendTransaction', methods=['POST'])
+    def fetch_transaction(self, transaction_hash):
+        # Placeholder implementation, fetch transaction data from database
+        return {"transaction_hash": transaction_hash, "details": "Transaction details"}
+
+    def fetch_balance(self, account_address):
+        # Placeholder implementation, fetch account balance from database
+        return {"account_address": account_address, "balance": 100}
+
+# Create instances of components
+validation_engine = ValidationEngine()
+mempool = Mempool()
+miner = Miner(mempool)
+storage_engine = StorageEngine()
+
+# Define API endpoints
+@app.route('/send_transaction', methods=['POST'])
 def send_transaction():
     data = request.json
     if 'transaction' in data:
         transaction = data['transaction']
-        mempool.add_transaction(transaction)
-        return jsonify({'message': 'Transaction added to mempool'})
+        transaction_hash = validation_engine.validate_and_add_transaction(transaction)
+        return jsonify({'message': 'Transaction added to mempool', 'transaction_hash': transaction_hash})
     else:
         return jsonify({'error': 'Transaction data not provided'})
 
-@app.route('/getBlockchain', methods=['GET'])
-def get_blockchain():
-    return jsonify(miner.blockchain)
+@app.route('/get_block/<block_hash>', methods=['GET'])
+def get_block(block_hash):
+    block_data = storage_engine.fetch_block(block_hash)
+    return jsonify(block_data)
+
+@app.route('/get_transaction/<transaction_hash>', methods=['GET'])
+def get_transaction(transaction_hash):
+    transaction_data = storage_engine.fetch_transaction(transaction_hash)
+    return jsonify(transaction_data)
+
+@app.route('/balance/<account_address>', methods=['GET'])
+def get_balance(account_address):
+    balance_data = storage_engine.fetch_balance(account_address)
+    return jsonify(balance_data)
 
 if __name__ == '__main__':
     # Start Flask app in a separate thread
     flask_thread = threading.Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 5000})
     flask_thread.start()
-    
+
     # Start the miner thread
     miner.start()
-    
+
     try:
         while True:
             pass  # Keep the main thread running
