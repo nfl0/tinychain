4c53e001f6bdc8f8149006528b226527ce17a1c8
nfl0 <nawfalona@gmail.com>
2024-10-29 17:26:50 +0100

    Add keypair generation to seed phrase generator script
    * Import `Wallet` class from `wallet.py`
    * Add code to generate keypair from seed phrase and save to "temp_wallet.dat"
    * Print wallet address for generated keypair

--- src/misc/seed_phrase_generator.py
+++ src/misc/seed_phrase_generator.py
@@ -1,4 +1,5 @@
 import mnemonic
+from wallet import Wallet
 
 def generate_seed_phrase():
     mnemo = mnemonic.Mnemonic("english")
@@ -8,3 +9,8 @@ def generate_seed_phrase():
 if __name__ == "__main__":
     seed_phrase = generate_seed_phrase()
     print("Generated Seed Phrase:", seed_phrase)
+
+    wallet = Wallet()
+    wallet.generate_keypair_from_mnemonic(seed_phrase, "temp_wallet.dat")
+    print("Generated Keypair for Seed Phrase:")
+    print("Wallet address:", wallet.get_address())
